32758f11adda585c641b93d68f64f7fb04a60d40
Katie Barrette <katiebarrette@katie-barrettes-macbook-2.local>
2017-11-15 13:33:12 -0500

    adding new custom form validation for name

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 
-///hide guesswho
+///hide guesswho (jumbotron text)
 $(".guesswho").hide().show("slow");
 
-//show guesswho-slow
+//show guesswho(jumbotron text)-slow
+
+
+// hide name validation error message
+$(function() {
+  $("#name_error_message").hide();
+
+
+var error_name = false;
+// run function to make sure there is text in name field when clicking out
+$("#name").focusout(function()
+{
+  // alert("Tell Bill your name.");
+  check_name();
+});
+
+// function to to check name is entered
+function check_name(){
+var name_length = $("#name").val().length;
+
+if(name_length < 1) {
+  $("#name_error_message").show();
+  error_name = true;
+} else {
+  $("#name_error_message").hide();
+}
+}
+});
